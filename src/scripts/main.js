252f007eab312eef35fb99d044aa72712e5bd53d
Heath Lester <h.allen.lester@gmail.com>
2020-10-12 10:19:35 -0500

    Added seed functions returning crop objects. Imported to main.js and logged to the console.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
-console.log("Welcome to the main module")
-
 import {  createPlan  } from './plan.js';
+import {  createAsparagus  } from './seeds/asparagus.js';
+import {  createCorn  } from './seeds/corn.js';
+import {  createPotato  } from './seeds/potato.js';
+import {  createSunflower  } from './seeds/sunflower.js';
+import {  createSoybean  } from './seeds/soybean.js';
+import {  createWheat  } from './seeds/wheat.js';
+
 
 const yearlyPlan = createPlan();
+const asparagusSeed = createAsparagus();
+const cornSeed = createCorn();
+const potatoSeed = createPotato();
+const sunflowerSeed = createSunflower();
+const soybeanSeed = createSoybean();
+const wheatSeed = createWheat();
 
+
+console.log("Welcome to the main module")
 console.log(yearlyPlan)
+console.log(asparagusSeed)
+console.log(potatoSeed)
+console.log(cornSeed)
+console.log(sunflowerSeed)
+console.log(soybeanSeed)
+console.log(wheatSeed)
